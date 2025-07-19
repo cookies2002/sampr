cedf3af3ffa9877b075178bafd5da47adf0f1f3e
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-07-19 16:33:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_0000767707259014901963' ) ## get your secret 
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
